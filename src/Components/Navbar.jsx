bf45db9b55bbc9d60d5f5a38af24dd6ce5c1714e
Fredericka <frederickatettey10@gmail.com>
2024-03-18 17:08:32 +0000

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav class="navbar">
+    <ul>
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">Gallery</a>
+        </li>
+        <li>
+            <a href="">About Us</a>
+        </li>
+        <li>
+            <a href="">Features</a>
+        </li>
+
+
+        <li>
+            <a href="">Contact Us</a>
+        </li>
+    </ul>
+
+
+
+</nav>
+  )
+}
